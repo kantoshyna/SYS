36a7a8b8c4a5ae1aad5ff2a18b75b01e1f394903
KatiaAntoshyna <akaterink@gmail.com>
2019-03-21 18:01:34 +0200

    made login window opening

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-var loginwindow = document.createElement('div');
+var loginwindow = $("#pro-logsite");
+
+$('#x').click(function () {
+    loginwindow.empty();
+});
 
 $("#login").click(function () {
-    loginwindow.setAttribute("id", "logsite");
-    loginwindow.innerHTML = '<div class="logwind">' +
-        '<span>Логін</span>' +
-        '  <input type="text">' +
-        '<br><span>Пароль</span>' +
-        '   <input type="password">' +
-        '      <br><button type="submit">Увійти</button>' +
-        '<a href="">Ще не зареєстровані?</a>' +
-        '</div>';
-    document.body.appendChild(loginwindow);
+    var logsite = document.createElement('div');
+    logsite.id = "logsite";
+    var logwind = document.createElement('div');
+    logwind.id = "logwind";
+    logwind.innerHTML = '<div style = "padding: 5% 5% 2%; border-bottom: 1px grey dotted" >' +
+        '<p style = "color: black; font-size:17px"> <b> Авторизація </b> <button class="submit" id="x" style="width: 30px"> x </button></p>' +
+
+        '</div>' +
+        '<div style=" padding: 5% ">' +
+        '  <span>Логін</span>' +
+        '<input type="text">  </div>' +
+
+        '   <div style=" padding: 5% 5% 7%"> <span>Пароль</span>' +
+        '      <input type="password">  </div>' +
+
+        '<div style="padding: 5%; border-top: 1px grey dotted">' +
+        '<a href="" style="color: black">Ще не зареєстровані?</a>' +
+        '<button type="submit" class="submit">Увійти</button>' +
+        '</div >';
+
+    logsite.append(logwind);
+    loginwindow.append(logsite);
 });
